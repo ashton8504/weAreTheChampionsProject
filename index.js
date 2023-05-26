c66e29c8c8e6b5965803b31ed19dd15ac92c5c73
ashton8504 <ashtonlittle8504@gmail.com>
2023-05-26 09:04:47 -0400

    updated

--- index.js
+++ index.js
@@ -13,22 +13,45 @@ const endorsementsQuotes = ref(database, "endorsements");
 const publishBtn = document.querySelector(".publishBtn");
 const inputBox = document.querySelector(".inputBox");
 const endorsementList = document.querySelector(".endorsementList");
+const errorMessage = document.querySelector(".errorMessage");
 
 // This allows user to add an endorsement
 publishBtn.addEventListener("click", () => {
   let inputValue = inputBox.value;
-  appendToEndorsements(inputValue);
-  clearInput();
+  if (inputValue.trim() !== "") {
+    appendToEndorsements(inputValue);
+    clearInput();
+    hideErrorMessage();
+  } else {
+    showErrorMessage("Please enter a comment.");
+  }
 });
 
 // This allows user to just press enter
 inputBox.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     let inputValue = inputBox.value;
-    appendToEndorsements(inputValue);
-    clearInput();
+    if (inputValue.trim() !== "") {
+      appendToEndorsements(inputValue);
+      clearInput();
+      hideErrorMessage();
+    } else {
+      showErrorMessage("Please enter a comment.");
+    }
   }
 });
+
+// Function to show error message
+function showErrorMessage(message) {
+  errorMessage.textContent = message;
+  errorMessage.style.display = "block";
+}
+
+// Function to hide error message
+function hideErrorMessage() {
+  errorMessage.textContent = "";
+  errorMessage.style.display = "none";
+}
 
 // Function to Clear input after submit
 function clearInput() {
