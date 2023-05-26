71fd5326aeb1ae5452e143c4af9f35cecb497cc2
ashton8504 <ashtonlittle8504@gmail.com>
2023-05-26 07:39:38 -0400

    updated
    Updated CSS for padding in input box and added a click on the button

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// javascript
+// imports from firebase
+
+import { initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database2-17bf3-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsementsQuotes = ref(database, "endorsements")
+
+const publishBtn = document.querySelector(".publishBtn")
+const inputBox = document.querySelector(".inputBox")
+
+
+publishBtn.addEventListener("click", () => {
+    // testing input
+    let inputValue = inputBox.value 
+    console.log(inputValue)
+})
