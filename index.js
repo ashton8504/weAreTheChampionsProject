acf4e7f582276aba56e3cee173c27736bfed3459
ashton8504 <ashtonlittle8504@gmail.com>
2023-05-26 08:22:55 -0400

    updated

--- index.js
+++ index.js
@@ -1,45 +1,60 @@
 // imports from firebase
-
-import { initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL: "https://realtime-database2-17bf3-default-rtdb.firebaseio.com/"
-}
+  databaseURL: "https://realtime-database2-17bf3-default-rtdb.firebaseio.com/"
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const endorsementsQuotes = ref(database, "endorsements")
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const endorsementsQuotes = ref(database, "endorsements");
 
-const publishBtn = document.querySelector(".publishBtn")
-const inputBox = document.querySelector(".inputBox")
+const publishBtn = document.querySelector(".publishBtn");
+const inputBox = document.querySelector(".inputBox");
+const endorsementList = document.querySelector(".endorsementList");
 
-// This allows user to add an endorsement 
+// This allows user to add an endorsement
 publishBtn.addEventListener("click", () => {
-    let inputValue = inputBox.value 
-    appendToEndorsements(inputValue)
-    clearInput()
-})
+  let inputValue = inputBox.value;
+  appendToEndorsements(inputValue);
+  clearInput();
+});
 
 // This allows user to just press enter
 inputBox.addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-      let inputValue = inputBox.value;
-      appendToEndorsements(inputValue);
-      clearInput();
-    }
-  });
-  
+  if (event.key === "Enter") {
+    let inputValue = inputBox.value;
+    appendToEndorsements(inputValue);
+    clearInput();
+  }
+});
 
 // Function to Clear input after submit
 function clearInput() {
-    inputBox.value = ""
+  inputBox.value = "";
 }
 
+// Function to append endorsement to the list
 function appendToEndorsements(text) {
-    const endorsementList = document.querySelector(".endorsementList");
+    const endorsementKey = push(endorsementsQuotes).key; // Generate a unique key for the endorsement
     const newEl = document.createElement("li");
     newEl.textContent = text;
     endorsementList.appendChild(newEl);
-  }
+    push(ref(database, `endorsements/${endorsementKey}`), text);
+    
+    // Remove endorsement on double-click
+    newEl.addEventListener("dblclick", () => {
+      remove(ref(database, `endorsements/${endorsementKey}`));
+      endorsementList.removeChild(newEl);
+    });
   
+    // Change background color on hover
+    newEl.addEventListener("mouseenter", () => {
+      newEl.style.backgroundColor = "#C82332";
+    });
+  
+    newEl.addEventListener("mouseleave", () => {
+      newEl.style.backgroundColor = "";
+    });
+}
